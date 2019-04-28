f946fcfe35be6b0aa207b648d8d06411ec5abbeb
ajabrahamsen1 <46762854+ajabrahamsen1@users.noreply.github.com>
2019-04-28 18:52:46 -0500

    Templates, statice, media

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(requeset):
-	return HttpResponse("Rango says hey there partner!")
+def index(request):
+	context = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+	return render(request, 'rango/index.html', context)
+	
+def about(request):
+	return HttpResponse('Rango says here is the about page.<br />Check out the about page -> <a href="/rango/">Index</a>')
