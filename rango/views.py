fe4a68d4f72b7b25cee7883b0784e74c57ecf323
ajabrahamsen1 <46762854+ajabrahamsen1@users.noreply.github.com>
2019-04-28 19:16:05 -0500

    Cat, about template

--- rango/views.py
+++ rango/views.py
@@ -6,4 +6,5 @@ def index(request):
 	return render(request, 'rango/index.html', context)
 	
 def about(request):
-	return HttpResponse('Rango says here is the about page.<br />Check out the about page -> <a href="/rango/">Index</a>')
+	context = {'boldmessage': 'This tutorial has been put together by AJ Abrahamsen.'}
+	return render(request, 'rango/about.html', context)
