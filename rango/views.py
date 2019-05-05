bba3074a40c4acb5a59b390642cc22ad8b850329
ajabrahamsen1 <46762854+ajabrahamsen1@users.noreply.github.com>
2019-05-05 15:48:15 -0500

    Slug
    Added category page, added slug field to Category table, and added hyperlinks to index category list.

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category, Page
 
 def index(request):
-	context = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+	category_list = Category.objects.order_by('-likes')[:5]
+	
+	context = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!', 'categories': category_list,}
+	
 	return render(request, 'rango/index.html', context)
 	
 def about(request):
 	context = {'boldmessage': 'This tutorial has been put together by AJ Abrahamsen.'}
 	return render(request, 'rango/about.html', context)
+	
+def show_category(request, category_name_slug):
+	context = {}
+	
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context['pages'] = pages
+		context['category'] = category
+	except Category.DoesNotExist:
+		context['pages'] = None
+		context['category'] = None
+	return render(request, 'rango/category.html', context)
