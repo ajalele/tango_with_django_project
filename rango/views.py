d4c90860870e089b608733065466a852a245af41
ajabrahamsen1 <46762854+ajabrahamsen1@users.noreply.github.com>
2019-05-06 18:34:13 -0500

    Pages
    Added top 5 viewed pages and changed index.html.

--- rango/views.py
+++ rango/views.py
@@ -4,8 +4,9 @@ from rango.models import Category, Page
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
+	page_list = Page.objects.order_by('-views')[:5]
 	
-	context = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!', 'categories': category_list,}
+	context = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!', 'categories': category_list, 'pages': page_list}
 	
 	return render(request, 'rango/index.html', context)
 	
